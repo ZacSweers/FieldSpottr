2bd50b3a572df75560309e76f20a7a0f261edc76
Zac Sweers <pandanomic@gmail.com>
2024-06-28 01:48:38 -0400

    Fix alignment

--- src/commonMain/kotlin/dev/zacsweers/fieldspottr/About.kt
+++ src/commonMain/kotlin/dev/zacsweers/fieldspottr/About.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -53,7 +54,7 @@ fun About(modifier: Modifier = Modifier) {
       modifier = modifier.fillMaxSize(),
       showAuthor = false,
       showVersion = false,
-      header = { item(key = "header") { Header() } },
+      header = { item(key = "header") { Header(Modifier.fillMaxWidth()) } },
     )
   }
 }
