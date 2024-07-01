edf80b5c921f121ba6e17c6bb239d11feb5dc2f9
Zac Sweers <pandanomic@gmail.com>
2024-07-01 15:50:45 -0400

    Remove bugsnag iOS for now (#15)
    This completely falls over in linking and is becoming a nuisance

--- FieldSpottr/FieldSpottr/FieldSpottrApp.swift
+++ FieldSpottr/FieldSpottr/FieldSpottrApp.swift
@@ -6,20 +6,10 @@
 //
 
 import SwiftUI
-import Bugsnag
 import FieldSpottrKt
 
 @main
 struct FieldSpottrApp: App {
-    init() {
-        if BuildConfig.shared.IS_RELEASE {
-            if let key = BuildConfig.shared.BUGSNAG_NOTIFIER_KEY {
-                let config = BugsnagConfiguration(_: key)
-                BugsnagConfigKt.startBugsnag(config: config)
-                BugsnagKotlinKt.enableBugsnag()
-            }
-        }
-    }
     var body: some Scene {
         WindowGroup {
             ContentView()
