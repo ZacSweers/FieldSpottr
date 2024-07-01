cc0e094f2d04d21c1c51ce5edb0f398c6f08046a
Zac Sweers <pandanomic@gmail.com>
2024-07-01 15:15:32 -0400

    Disable bugsnag for now

--- FieldSpottr/FieldSpottr/FieldSpottrApp.swift
+++ FieldSpottr/FieldSpottr/FieldSpottrApp.swift
@@ -6,20 +6,21 @@
 //
 
 import SwiftUI
-import Bugsnag
+// import Bugsnag
 import FieldSpottrKt
 
 @main
 struct FieldSpottrApp: App {
-    init() {
-        if BuildConfig.shared.IS_RELEASE {
-            if let key = BuildConfig.shared.BUGSNAG_NOTIFIER_KEY {
-                let config = BugsnagConfiguration(_: key)
-                BugsnagConfigKt.startBugsnag(config: config)
-                BugsnagKotlinKt.enableBugsnag()
-            }
-        }
-    }
+    // TODO disabled for now because Bugsnag isn't getting linked correctly in release builds
+//    init() {
+//        if BuildConfig.shared.IS_RELEASE {
+//            if let key = BuildConfig.shared.BUGSNAG_NOTIFIER_KEY {
+//                let config = BugsnagConfiguration(_: key)
+//                BugsnagConfigKt.startBugsnag(config: config)
+//                BugsnagKotlinKt.enableBugsnag()
+//            }
+//        }
+//    }
     var body: some Scene {
         WindowGroup {
             ContentView()
