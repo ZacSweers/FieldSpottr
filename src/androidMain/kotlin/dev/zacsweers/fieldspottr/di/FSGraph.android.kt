6c1c8bd4106fe1e97c75501d689713d7ea628f1a
Zac Sweers <pandanomic@gmail.com>
2026-02-04 00:30:08 -0500

    Switch to app injection

--- src/androidMain/kotlin/dev/zacsweers/fieldspottr/di/FSGraph.android.kt
+++ src/androidMain/kotlin/dev/zacsweers/fieldspottr/di/FSGraph.android.kt
@@ -6,10 +6,8 @@ import android.content.Context
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.DependencyGraph
 import dev.zacsweers.metro.Provides
-import dev.zacsweers.metro.SingleIn
 
 @DependencyGraph(AppScope::class)
-@SingleIn(AppScope::class)
 interface AndroidFSGraph : FSGraph {
   @DependencyGraph.Factory
   fun interface Factory {
