db0b401326b218a101c91dc043c80f0fde7abc34
Zac Sweers <pandanomic@gmail.com>
2026-06-04 12:51:53 -0400

    Extract UA

---
+++ shared/src/commonMain/kotlin/dev/zacsweers/fieldspottr/data/NycParksApi.kt
@@ -0,0 +1,6 @@
+// Copyright (C) 2026 Zac Sweers
+// SPDX-License-Identifier: Apache-2.0
+package dev.zacsweers.fieldspottr.data
+
+internal const val NYC_PARKS_USER_AGENT =
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
